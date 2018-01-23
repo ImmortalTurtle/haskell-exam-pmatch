664a7199990e6d5ed48bf10802972e0c04de7493
ImmortalTurtle <immortalsuperturtle@gmail.com>
2018-01-23 19:10:52 +0300

    Support fields and tags

--- PMEval.hs
+++ PMEval.hs
@@ -41,22 +41,9 @@ data EvalRez =
                      -- unbound variables, etc.
   | PMatchFail       -- patterns are not exhaustive.
   deriving (Show,Eq)
-      
--- implement this function :: expr -> [(pattern,expr)] -> EvalRez
--- which tries to match `expr` using specified patterns and right-hand-sides
--- and returns appropritate answer. For, example
---    eval `parseScrutinee optsE "A"` [`parseCase optsp optsE "A -> 42"`]
---      should return (OK 42)
--- and
---    eval `parseScrutinee optsE "A"` []
---      should fail with PMatchFail because pattern matching is not exhaustive.
 
--- For examples about which expression and patterns can be written see tests file.
-
--- takes 2 constants and returns Const Int or BoolExpr
+-- takes 2 constants and returns Const Int, BoolExpr or InvalidExpr in case of error
 bincalc :: BinOpSort -> Expr -> Expr -> Expr
--- bincalc _ Nothing _ = InvalidExpr
--- bincalc _ _ Nothing = InvalidExpr
 bincalc oper (Const x) (Const y) = case oper of
     Mul -> Const $ x * y
     Add -> Const $ x + y
@@ -66,6 +53,7 @@ bincalc oper (Const x) (Const y) = case oper of
     LessEq -> BoolExpr $ x < y
 bincalc _ _ _ = InvalidExpr
 
+--
 subst :: Expr -> (Pattern, Expr) -> Expr
 subst what (Named var, Const num) = Const num
 subst what (Named pvar, Var evar) = if pvar == evar then what else (Var evar)
@@ -76,55 +64,64 @@ subst what (Named var, Econstr name args) = Econstr name $ map (curry (subst wha
 subst what (Named var, Ifthenelse cond e1 e2) = Ifthenelse (subst what (Named var, cond))
                                                            (subst what (Named var, e1))
                                                            (subst what (Named var, e2))
+-- subst (Econstr name eargs) ((Pconstr name pargs), e) = Econstr 
+
 subst what (p, e) = e
 
---reduce1 may return error
 reduce1 :: Expr -> Maybe Expr
 reduce1 (Const _) = Nothing 
 reduce1 (Var _) = Nothing --substitute
 reduce1 (Tag (Econstr name _)) = Just (Const $ hash name)
--- reduce1 (Field index (Econstr string args)) = args !! index --not Econstr | len(args) < index -> BadProgram
-reduce1 (BinOp oper e1 e2) = Just (bincalc oper (simplify reduce1 e1) (simplify reduce1 e2))
+reduce1 (Field index cons@(Econstr string args)) = case lookup' args index of  
+    Just e -> Just e
+    Nothing -> Just InvalidExpr
+reduce1 (Field index _) = Just InvalidExpr
+reduce1 (BinOp oper e1 e2) = Just (bincalc oper (simplify e1) (simplify e2))
 reduce1 (Ifthenelse (BoolExpr condition) e1 e2) = if condition then Just e1 else Just e2 
---TODO: need to simplify possible condition as expression
+reduce1 (Ifthenelse condexpr e1 e2) = case (reduce1 condexpr) of
+    Just (BoolExpr bool)-> if bool then Just e1 else Just e2
+    -- InvalidExpr -> Just InvalidExpr
+    _ -> Nothing
 reduce1 _ = Just InvalidExpr
--- reduce1 (Ifthenelse condexpr e1 e2) = reduce1 (Ifthenelse (simplify))
 
-    
-simplify ::(Expr -> Maybe Expr) ->  Expr -> Expr
-simplify reduce1 InvalidExpr = InvalidExpr
-simplify reduce1 e = case reduce1 e of
-                         Just e -> simplify reduce1 e
-                         Nothing -> e 
 
--- ((Expr, Pattern) -> Bool) -> [(Expr, Pattern)] -> Bool
--- (a -> Bool) -> [a] -> Bool
--- curry match' 
---check if expr matches pattern, reduce if needed
+--copypaste
+lookup' :: [a] -> Int -> Maybe a
+lookup' [] _ = Nothing
+lookup' (a:as) 0 = Just a
+lookup' (a:as) n = lookup' as (n - 1)
+
+
+simplify :: Expr -> Expr
+simplify InvalidExpr = InvalidExpr
+simplify e = case reduce1 e of
+                 Just e -> simplify e
+                 Nothing -> e 
+
+--check for Expr-Pattern match
 match' :: Expr -> Pattern -> Bool
 match' _ Wild = True
 match' (Const e) (Pconst p) = e == p
+match' (Const e) (Named var) = True
 match' (Econstr ename eargs) (Pconstr pname pargs) = ename == pname && length eargs == length pargs
                                                      && (all (uncurry match') (zip eargs pargs))
+match' x (Named var) = True
+match' InvalidExpr _ = False --or True and then able to throw BadProgram
+match' e p = case reduce1 e of
+    Nothing -> False
+    Just e -> match' e p
 match' _ _ = False
---all match cases
---if not found, try to reduce Expr
---not reducable => return False
 
---this is correct pattern, just calculate the expression, which is already simplified
+--called if match succeeded
 calculate :: Expr -> (Pattern, Expr) -> EvalRez
--- calculate what (Wild, _) = OK 43 --testWild
-calculate what (Pconstr _ _, e) = OK 41 --testConstr
-calculate what (p, e) = case simplify reduce1 (subst what (p, e)) of
+calculate what (p, e) = case simplify (subst what (p, e)) of
     Const a -> OK a
     InvalidExpr -> BadProgram
-    _ -> PMatchFail
-calculate _ _ = OK 42 
+    _ -> PMatchFail 
 
-
+--main
 eval ::  Expr -> [(Pattern, Expr)] -> EvalRez
 eval _ [] = PMatchFail
 eval expr ((lhs,rhs) : ps) = 
     if (match' expr lhs) then (calculate expr (lhs,rhs))
     else eval expr ps
-    -- simplify all expressions
--- test-pmlang.hs
+++ test-pmlang.hs
@@ -37,13 +37,13 @@ tests = TestList
   , wrap "test7"   "P(10,10)" ["P(x,y) -> if 6<x then (if 19<y then 42 else 19) else 34" ] (OK 42)
   , wrap "test8"   "10" ["10 -> 42" ] (OK 42)
   -- next should fail with silly eval
-  --, wrap "test101" "A(B,C)"   ["A(x,y) -> (tag x) + (tag y)"] (OK $ hash "B" + hash "C")
-  --, wrap "test102" "A(1,2)"   ["A(2,x) -> 42"
-  --                            ,"A(x,1) -> 42"
-  --                            ,"3      -> 42"
-  --                            ,"A(2,1) -> 42"
-  --                            ]
-  --      PMatchFail
+  , wrap "test101" "A(B,C)"   ["A(x,y) -> (tag x) + (tag y)"] (OK $ hash "B" + hash "C")
+  , wrap "test102" "A(1,2)"   ["A(2,x) -> 42"
+                             ,"A(x,1) -> 42"
+                             ,"3      -> 42"
+                             ,"A(2,1) -> 42"
+                             ]
+       PMatchFail
   , wrap "test101" "A(B,C)"   ["x -> y"] BadProgram
   ]
 
